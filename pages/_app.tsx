860c23795acf067e5ad568a709f30567be17292f
andreswebtronic <andres.m@webtroniclabs.com>
2023-03-21 11:20:48 -0600

    remove aws exports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,10 @@ import type { AppProps } from 'next/app'
 import '../configureAmplify'
 import {NavBar} from '../pages/components/Navbar'
 
+
+
 export default function App({ Component, pageProps }: AppProps) {
+
   return(
     <>
     <NavBar></NavBar>
